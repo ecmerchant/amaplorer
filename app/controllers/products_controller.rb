1a2b47d9b2528bd020646733fa98d6b14a8423f4
ecmerchant <ecmerchant.contact@gmail.com>
2018-07-12 02:22:50 +0900

    fix csv

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -111,7 +111,7 @@ class ProductsController < ApplicationController
       logger.debug(stream)
       t = Time.now
       strTime = t.strftime("%Y%m%D%H%M%S")
-      fname = "出品CSV_" + strTime + ".csv"
+      fname = "出品CSV_" + strTime + ".tsv"
       send_data(stream, filename: fname)
     else
       redirect_to products_search_path
