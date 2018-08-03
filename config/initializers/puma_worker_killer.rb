03004d3f6259e456d8d7dbefdeef8f1140b07fd0
ecmerchant <ecmerchant.contact@gmail.com>
2018-08-03 20:54:35 +0900

    puma

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,14 +1,1 @@
-before_fork do
-  require 'puma_worker_killer'
-  PumaWorkerKiller.config do |config|
-    config.ram           = 512 # mb
-    config.frequency     = 5    # seconds
-    config.percent_usage = 0.98
-    config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
-    config.reaper_status_logs = true # setting this to false will not log lines like:
-    # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
-
-    config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
-  end
-  PumaWorkerKiller.start
-end
+PumaWorkerKiller.enable_rolling_restart(1 * 3600) # 8 hours in seconds
