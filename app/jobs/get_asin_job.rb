3187fc1016433c8868f32d0faf607cb2e5e32506
ecmerchant <ecmerchant.contact@gmail.com>
2018-08-04 22:22:59 +0900

    add constrains

--- app/jobs/get_asin_job.rb
+++ app/jobs/get_asin_job.rb
@@ -26,32 +26,31 @@ class GetAsinJob < ApplicationJob
     cc = 0
 
     casins = Hash.new
-
     ulevel = account.user_level
     counter = 0
     d = DateTime.now
     uid = d.strftime("%Y%m%d%H%M%S")
-    tu = Account.find_by(user: user)
-    tu.update(unique_id: uid)
+    #tu = Account.find_by(user: user)
+    account.update(unique_id: uid)
+    asin_list = Array.new
+
+    ua = CSV.read('app/others/User-Agent.csv', headers: false, col_sep: "\t")
+    uanum = ua.length
+    user_agent = ua[rand(uanum)][0]
+    logger.debug("user_agent:" + user_agent)
 
     if condition == 'from_url' then
       #ASINの入力方法:URLの場合
       logger.debug('条件：URL')
       org_url = arg2
-      asin = Array.new
+      #asin = Array.new
       #URLからASINリストの作成
       loop do
         begin
-
           url = org_url + '&page=' + i.to_s
           logger.debug("URL：" + url)
 
-          ua = CSV.read('app/others/User-Agent.csv', headers: false, col_sep: "\t")
-          uanum = ua.length
-          user_agent = ua[rand(uanum)][0]
-          logger.debug("user_agent:" + user_agent)
           sleep(1.1)
-
           cc = 0
           begin
             html = open(url, "User-Agent" => user_agent) do |f|
@@ -69,12 +68,10 @@ class GetAsinJob < ApplicationJob
           end
 
           doc = Nokogiri::HTML.parse(html, nil)
-
           asins = doc.css('li/@data-asin')
 
           #終了条件1：検索結果がヒットしない
           hbody = html.force_encoding("UTF-8")
-
           rnum = hbody.match(/<span id="s-result-count">([\s\S]*?)</)
           logger.debug("=====================")
           if rnum != nil then
@@ -93,13 +90,9 @@ class GetAsinJob < ApplicationJob
             break
           end
 
-          asin_list = Array.new
-
           asins.each do |temp_asin|
-
-            asin.push(temp_asin)
+            #asin.push(temp_asin)
             tag = temp_asin.to_s
-
             if casins.key?(tag) == false then
               asin_list << Product.new(user:user, asin:tag, unique_id: uid, isvalid: true)
               casins[tag] = ecounter
@@ -107,10 +100,8 @@ class GetAsinJob < ApplicationJob
             end
 
             logger.debug(tag)
-
             #asin_list << Product.new(user:user, asin:tag, unique_id: uid, isvalid: true)
             #asin_list << Product.new(asin:tag)
-
             #temp = tproduct.find_or_create_by(asin:tag)
             #temp.update(unique_id: uid, isvalid: true)
 
@@ -123,21 +114,15 @@ class GetAsinJob < ApplicationJob
           end
           #メモリ開放用
           account.update(asin_status: "実行中 " + ecounter.to_s + "件済")
-
-          #Product.import asin_list
-          Product.import asin_list, on_duplicate_key_update: {constraint_name: :for_upsert, columns: [:unique_id, :isvalid]}
-          asin_list = nil
-          logger.debug("\n====== GC START =========")
-          ObjectSpace.each_object(ActiveRecord::Relation).each(&:reset)
-          GC.start
         rescue => e
           logger.debug("エラーあり")
           logger.debug(e)
           break
         end
+        doc = nil
+        asins = nil
         i += 1
       end
-
     elsif condition == 'from_file' then
       #ASINの入力方法:ファイルからの場合
       asin = Array.new
@@ -147,21 +132,17 @@ class GetAsinJob < ApplicationJob
         asin.push(row[0].to_s.gsub("\n","").strip())
       end
       counter = 0
-
       asin.each do |tasin|
         tag = tasin.to_s
         logger.debug(tag)
-
         if casins.key?(tag) == false then
           asin_list << Product.new(user:user, asin:tag, unique_id: uid, isvalid: true)
           casins[tag] = ecounter
           ecounter += 1
         end
-
         #temp = tproduct.find_or_create_by(asin:tag)
         #ecounter += 1
         #temp.update(unique_id: uid, isvalid: true)
-
 
         if ulevel == "trial" then
           counter += 1
@@ -170,15 +151,16 @@ class GetAsinJob < ApplicationJob
           end
         end
       end
-      Product.import asin_list, on_duplicate_key_update: {constraint_name: :for_upsert, columns: [:unique_id, :isvalid]}
-      asin_list = nil
       account.update(asin_status: "実行中 " + ecounter.to_s + "件済")
-      #メモリ開放用
-      logger.debug("\n====== GC START =========")
-      ObjectSpace.each_object(ActiveRecord::Relation).each(&:reset)
-      GC.start
-      logger.debug(asin)
     end
+
+    Product.import asin_list, on_duplicate_key_update: {constraint_name: :for_upsert, columns: [:unique_id, :isvalid]}
+    asin_list = nil
+    casins = nil
+    #メモリ開放用
+    logger.debug("\n====== GC START =========")
+    ObjectSpace.each_object(ActiveRecord::Relation).each(&:reset)
+    GC.start
 
     logger.debug('======= GET ASIN END =========')
     account.update(asin_status: "完了 " + ecounter.to_s + "件済")
@@ -189,11 +171,5 @@ class GetAsinJob < ApplicationJob
       account.cw_api_token,
       account.cw_room_id
     )
-
-    #メモリ開放用
-    logger.debug("\n====== GC START =========")
-    ObjectSpace.each_object(ActiveRecord::Relation).each(&:reset)
-    GC.start
-    #GetItemDataJob.perform_later(current_user.email, uid)
   end
 end
