d1f509fa982893f8afe06531397c0e683f836c03
ecmerchant <ecmerchant.contact@gmail.com>
2018-08-04 21:05:52 +0900

    add constrains

--- db/migrate/20180804114852_add_constrains_to_product.rb
+++ db/migrate/20180804114852_add_constrains_to_product.rb
@@ -1,6 +1,6 @@
 class AddConstrainsToProduct < ActiveRecord::Migration[5.0]
   def change
-    sql = "ALTER TABLE products ADD CONSTRAINT for_upsert UNIQUE (user, asin);"
+    sql = 'ALTER TABLE products ADD CONSTRAINT for_upsert UNIQUE ("user", asin);'
     ActiveRecord::Base.connection.execute(sql)
   end
 end
