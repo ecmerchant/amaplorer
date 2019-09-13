dac7654d331b0744e55de9f5d7a6b586d7d2ad1c
ecmerchant <murakami@ec-merchant.com>
2019-09-13 10:11:56 +0900

    upd

--- app/jobs/get_item_data_job.rb
+++ app/jobs/get_item_data_job.rb
@@ -1,5 +1,5 @@
 class GetItemDataJob < ApplicationJob
-  queue_as :default
+  queue_as :get_item_data
 
   rescue_from(StandardError) do |exception|
    # Do something with the exception
@@ -8,6 +8,8 @@ class GetItemDataJob < ApplicationJob
 
   def perform(user, uid)
     # Do something later
+    account = Account.find_by(user: user)
+    account.update(amazon_status: "実行中 0%", yahoo_status: "準備中")
     a = Product.new
     a.amazon(user, uid)
     #a.yahoo_shopping(user, uid)
